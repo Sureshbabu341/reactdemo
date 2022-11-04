43c05b5bc3d6734d82b9fafba3956db06b0caafd
sureshbabu8 <asureshtcs@gmail.com>
2022-11-04 13:38:14 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,16 +3,13 @@ import {connect} from 'react-redux';
 import {IncAction} from './actions';
 import {DecAction} from './actions';
 import Hoc from './HOC';
-import ObjectData from './ObjectData'
+//import ObjectData from './ObjectData';
 
 const App = ({local_variable, IncAction, DecAction}) => {
   return(
     <div>{local_variable}<br />
-
     <button onClick={() => IncAction(1)}>Piki</button>
     <button onClick={() => DecAction(1)}>Kindaki</button>
-
-    <div><ObjectData /></div>
     </div>
   )
 }
